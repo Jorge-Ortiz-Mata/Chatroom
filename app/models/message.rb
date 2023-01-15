4954e6377ccde7989362fc2f92fcb988b3d96fd9
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-15 17:36:01 -0600

    Turbo Stream working.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :room
+
+  after_commit on: :create do
+    broadcast_append_to(
+      @messages_channel,
+      partial: 'messages/message',
+      locals: {message: self},
+      target: "room_messages_#{self.room.id}"
+    )
+  end
 end
