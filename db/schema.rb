24b688e3de1fbd468ebba3b3188aacfcd37fa76c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-15 17:06:28 -0600

    Messages model and association.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_230254) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_230539) do
+  create_table "messages", force: :cascade do |t|
+    t.string "name"
+    t.integer "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
 end
