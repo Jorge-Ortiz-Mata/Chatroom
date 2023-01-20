4a2d9dbb3ce31eb1d65625912db0dc4df34a2980
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-20 01:32:57 +0000

    Adding room_details_channel

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,12 @@ class Room < ApplicationRecord
   has_many :messages
   has_many :room_users
   has_many :users, through: :room_users
+
+  after_update_commit  :update_room_details
+
+  private
+
+  def update_room_details
+    broadcast_replace_to('room_details_channel', partial: 'sidebar/single_room', locals: {room: self}, target: "room_#{id}")
+  end
 end
